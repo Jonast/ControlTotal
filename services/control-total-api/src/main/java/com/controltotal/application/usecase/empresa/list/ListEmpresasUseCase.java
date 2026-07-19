92847908d864630dbf6c7dea1fc7e1eaead2f20f
Jonast <jonas233323@gmail.com>
2026-07-18 21:59:04 -0400

    feat: crear endpoint para listar empresas

--- services/control-total-api/src/main/java/com/controltotal/application/usecase/empresa/list/ListEmpresasUseCase.java
+++ services/control-total-api/src/main/java/com/controltotal/application/usecase/empresa/list/ListEmpresasUseCase.java
@@ -1,2 +1,8 @@
-package com.controltotal.application.usecase.empresa.list;public class ListEmpresasUseCase {
+package com.controltotal.application.usecase.empresa.list;
+
+import com.controltotal.application.dto.response.EmpresaResponse;
+import java.util.List;
+
+public interface ListEmpresasUseCase {
+    List<EmpresaResponse> execute();
 }
